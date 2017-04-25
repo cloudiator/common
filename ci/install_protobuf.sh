2bfca92111ed6aca8c99564c78efb64ac0a03047
Daniel Baur <daniel.baur@uni-ulm.de>
2017-04-25 15:43:06 +0200

    Used prebuilt protoc version

--- ci/install_protobuf.sh
+++ ci/install_protobuf.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-set -ex
-wget https://github.com/google/protobuf/archive/v3.2.0.tar.gz
-tar -xzvf v3.2.0.tar.gz
-cd protobuf-3.2.0
-./autogen.sh
-./configure --prefix=/usr
-make
-sudo make install
+## Build protobuf from sources
+
+#set -ex
+#wget https://github.com/google/protobuf/archive/v3.2.0.tar.gz
+#tar -xzvf v3.2.0.tar.gz
+#cd protobuf-3.2.0
+#./autogen.sh
+#./configure --prefix=/usr
+#make
+#sudo make install
+
+## Download prebuilt executable
+
+
