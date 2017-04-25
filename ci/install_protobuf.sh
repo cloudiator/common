d15662846c9788a4031bff2996ce6a63ff4eae00
Daniel Baur <daniel.baur@uni-ulm.de>
2017-04-25 15:16:29 +0200

    Added script to install protobuf

---
+++ ci/install_protobuf.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -ex
+wget https://github.com/google/protobuf/archive/v3.2.0.tar.gz
+tar -xzvf v3.2.0.tar.gz
+cd protobuf-3.2.0 && ./configure --prefix=/usr && make && sudo make install
