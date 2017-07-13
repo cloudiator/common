e9af0207503487f972a99377ea780abbadbbc035
Volker <volker.foth@uni-ulm.de>
2017-07-13 14:53:36 +0200

    add OperatingSystem messages

--- domain/src/main/java/de/uniulm/omi/cloudiator/domain/OperatingSystemFamily.java
+++ domain/src/main/java/de/uniulm/omi/cloudiator/domain/OperatingSystemFamily.java
@@ -28,96 +28,101 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public enum OperatingSystemFamily implements RemotePortProvider, LoginNameSupplier {
 
-    UNKNOWN,
+  UNKNOWN,
 
-    /**
-     * IBM AIX
-     */
-    AIX, /**
-     * Arch Linux
-     */
-    ARCH, /**
-     * Centos
-     */
-    CENTOS(OperatingSystemType.LINUX, OperatingSystemVersionFormats.set(2,3,4,5,6,7),
-            LoginNameSuppliers.staticSupplier("centos"), null), /**
-     * Darwin OS
-     */
-    DARWIN, DEBIAN, /**
-     * VMWare ESX
-     */
-    ESX, FEDORA, FREEBSD, GENTOO, /**
-     * Hewlett Packard Unix
-     */
-    HPUX, COREOS, /**
-     * Amazon Linux
-     */
-    AMZN_LINUX, MANDRIVA, NETBSD, /**
-     * Oracle Linux OS
-     */
-    OEL, OPENBSD, /**
-     * Red Hat Enterprise Linux
-     */
-    RHEL, SCIENTIFIC, CEL, SLACKWARE, SOLARIS, SUSE, TURBOLINUX, CLOUD_LINUX, UBUNTU(
-        OperatingSystemType.LINUX,
-        OperatingSystemVersionFormats.supplier(new UbuntuOperatingSystemVersionSupplier()),
-        LoginNameSuppliers.staticSupplier("ubuntu"), new UbuntuUrlDownloadFunction()), WINDOWS(
-        OperatingSystemType.WINDOWS, OperatingSystemVersionFormats.unknown(),
-        LoginNameSuppliers.staticSupplier("administrator"), null);
+  /**
+   * IBM AIX
+   */
+  AIX, /**
+   * Arch Linux
+   */
+  ARCH, /**
+   * Centos
+   */
+  CENTOS(OperatingSystemType.LINUX, OperatingSystemVersionFormats.set(2, 3, 4, 5, 6, 7),
+          LoginNameSuppliers.staticSupplier("centos"), null), /**
+   * Darwin OS
+   */
+  DARWIN, DEBIAN, /**
+   * VMWare ESX
+   */
+  ESX, FEDORA, FREEBSD, GENTOO, /**
+   * Hewlett Packard Unix
+   */
+  HPUX, COREOS, /**
+   * Amazon Linux
+   */
+  AMZN_LINUX, MANDRIVA, NETBSD, /**
+   * Oracle Linux OS
+   */
+  OEL, OPENBSD, /**
+   * Red Hat Enterprise Linux
+   */
+  RHEL, SCIENTIFIC, CEL, SLACKWARE, SOLARIS, SUSE, TURBOLINUX, CLOUD_LINUX,
+  UBUNTU(
+      OperatingSystemType.LINUX,
+      OperatingSystemVersionFormats.supplier(new UbuntuOperatingSystemVersionSupplier()),
+      LoginNameSuppliers.staticSupplier("ubuntu"), new UbuntuUrlDownloadFunction()),
+  WINDOWS(
+      OperatingSystemType.WINDOWS, OperatingSystemVersionFormats.unknown(),
+      LoginNameSuppliers.staticSupplier("administrator"), null);
 
-    private static final OperatingSystemFamily DEFAULT = UNKNOWN;
-    private final OperatingSystemType operatingSystemType;
-    private final OperatingSystemVersionFormat operatingSystemVersionFormat;
-    private final LoginNameSupplier loginNameSupplier;
-    @Nullable private final DownloadUrlFunction downloadUrlFunction;
+  private static final OperatingSystemFamily DEFAULT = UNKNOWN;
+  private final OperatingSystemType operatingSystemType;
+  private final OperatingSystemVersionFormat operatingSystemVersionFormat;
+  private final LoginNameSupplier loginNameSupplier;
+  @Nullable
+  private final DownloadUrlFunction downloadUrlFunction;
 
-    OperatingSystemFamily(OperatingSystemType operatingSystemType,
-        OperatingSystemVersionFormat operatingSystemVersionFormat,
-        LoginNameSupplier loginNameSupplier, @Nullable DownloadUrlFunction downloadUrlFunction) {
-        this.operatingSystemType = operatingSystemType;
-        this.operatingSystemVersionFormat = operatingSystemVersionFormat;
-        this.loginNameSupplier = loginNameSupplier;
-        this.downloadUrlFunction = downloadUrlFunction;
+  OperatingSystemFamily(OperatingSystemType operatingSystemType,
+      OperatingSystemVersionFormat operatingSystemVersionFormat,
+      LoginNameSupplier loginNameSupplier, @Nullable DownloadUrlFunction downloadUrlFunction) {
+    this.operatingSystemType = operatingSystemType;
+    this.operatingSystemVersionFormat = operatingSystemVersionFormat;
+    this.loginNameSupplier = loginNameSupplier;
+    this.downloadUrlFunction = downloadUrlFunction;
+  }
+
+  OperatingSystemFamily() {
+    this.operatingSystemType = OperatingSystemType.DEFAULT;
+    this.operatingSystemVersionFormat = OperatingSystemVersionFormats.unknown();
+    this.loginNameSupplier = LoginNameSuppliers.nullSupplier();
+    this.downloadUrlFunction = null;
+  }
+
+  public String value() {
+    return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_HYPHEN, this.name());
+  }
+
+  @Override
+  public int remotePort() {
+    return operatingSystemType.remotePort();
+  }
+
+  public OperatingSystemVersionFormat operatingSystemVersionFormat() {
+    return operatingSystemVersionFormat;
+  }
+
+  public static OperatingSystemFamily fromValue(String operatingSystemFamily) {
+    checkNotNull(operatingSystemFamily);
+    try {
+      return valueOf(
+          CaseFormat.LOWER_HYPHEN.to(CaseFormat.UPPER_UNDERSCORE, operatingSystemFamily));
+    } catch (IllegalArgumentException e) {
+      return DEFAULT;
     }
+  }
 
-    OperatingSystemFamily() {
-        this.operatingSystemType = OperatingSystemType.DEFAULT;
-        this.operatingSystemVersionFormat = OperatingSystemVersionFormats.unknown();
-        this.loginNameSupplier = LoginNameSuppliers.nullSupplier();
-        this.downloadUrlFunction = null;
-    }
+  public OperatingSystemType operatingSystemType() {
+    return operatingSystemType;
+  }
 
-    public String value() {
-        return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_HYPHEN, this.name());
-    }
+  public Optional<DownloadUrlFunction> downloadUrlFunction() {
+    return Optional.ofNullable(downloadUrlFunction);
+  }
 
-    @Override public int remotePort() {
-        return operatingSystemType.remotePort();
-    }
-
-    public OperatingSystemVersionFormat operatingSystemVersionFormat() {
-        return operatingSystemVersionFormat;
-    }
-
-    public static OperatingSystemFamily fromValue(String operatingSystemFamily) {
-        checkNotNull(operatingSystemFamily);
-        try {
-            return valueOf(
-                CaseFormat.LOWER_HYPHEN.to(CaseFormat.UPPER_UNDERSCORE, operatingSystemFamily));
-        } catch (IllegalArgumentException e) {
-            return DEFAULT;
-        }
-    }
-
-    public OperatingSystemType operatingSystemType() {
-        return operatingSystemType;
-    }
-
-    public Optional<DownloadUrlFunction> downloadUrlFunction() {
-        return Optional.ofNullable(downloadUrlFunction);
-    }
-
-    @Override public String loginName() {
-        return loginNameSupplier.loginName();
-    }
+  @Override
+  public String loginName() {
+    return loginNameSupplier.loginName();
+  }
 }
