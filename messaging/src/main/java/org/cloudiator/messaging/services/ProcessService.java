068550399515a1341c30d112561384c06475cb40
Daniel Baur <daniel.baur@uni-ulm.de>
2018-07-10 10:08:31 +0200

    Added process messages and service

--- messaging/src/main/java/org/cloudiator/messaging/services/ProcessService.java
+++ messaging/src/main/java/org/cloudiator/messaging/services/ProcessService.java
@@ -1,8 +1,12 @@
 package org.cloudiator.messaging.services;
 
+import org.cloudiator.messages.Process.CreateLanceProcessRequest;
+import org.cloudiator.messages.Process.CreateProcessRequest;
 import org.cloudiator.messages.Process.CreateScheduleRequest;
+import org.cloudiator.messages.Process.ProcessCreatedResponse;
 import org.cloudiator.messages.Process.ScheduleCreatedResponse;
 import org.cloudiator.messaging.MessageCallback;
+import org.cloudiator.messaging.ResponseCallback;
 import org.cloudiator.messaging.ResponseException;
 
 public interface ProcessService {
@@ -10,6 +14,21 @@ public interface ProcessService {
   ScheduleCreatedResponse createSchedule(CreateScheduleRequest createScheduleRequest)
       throws ResponseException;
 
+  ProcessCreatedResponse createProcess(CreateProcessRequest createProcessRequest)
+      throws ResponseException;
+
+  void createProcessAsync(CreateProcessRequest createProcessRequest,
+      ResponseCallback<ProcessCreatedResponse> callback);
+
+  void subscribeCreateProcessRequest(MessageCallback<CreateProcessRequest> callback);
+
+  ProcessCreatedResponse createLanceProcess(CreateLanceProcessRequest createLanceProcessRequest)
+      throws ResponseException;
+
+  void createLanceProcessAsync(CreateLanceProcessRequest createLanceProcessRequest,
+      ResponseCallback<ProcessCreatedResponse> callback) throws ResponseException;
+
+  void subscribeCreateLanceProcessRequest(MessageCallback<CreateLanceProcessRequest> callback);
 
   void subscribeSchedule(MessageCallback<CreateScheduleRequest> callback);
 }
--- messaging/src/main/java/org/cloudiator/messaging/services/ProcessServiceImpl.java
+++ messaging/src/main/java/org/cloudiator/messaging/services/ProcessServiceImpl.java
@@ -2,10 +2,14 @@ package org.cloudiator.messaging.services;
 
 import com.google.inject.Inject;
 import javax.inject.Named;
+import org.cloudiator.messages.Process.CreateLanceProcessRequest;
+import org.cloudiator.messages.Process.CreateProcessRequest;
 import org.cloudiator.messages.Process.CreateScheduleRequest;
+import org.cloudiator.messages.Process.ProcessCreatedResponse;
 import org.cloudiator.messages.Process.ScheduleCreatedResponse;
 import org.cloudiator.messaging.MessageCallback;
 import org.cloudiator.messaging.MessageInterface;
+import org.cloudiator.messaging.ResponseCallback;
 import org.cloudiator.messaging.ResponseException;
 
 public class ProcessServiceImpl implements ProcessService {
@@ -25,6 +29,42 @@ public class ProcessServiceImpl implements ProcessService {
   public ScheduleCreatedResponse createSchedule(CreateScheduleRequest createScheduleRequest)
       throws ResponseException {
     return messageInterface.call(createScheduleRequest, ScheduleCreatedResponse.class, timeout);
+  }
+
+  @Override
+  public ProcessCreatedResponse createProcess(CreateProcessRequest createProcessRequest)
+      throws ResponseException {
+    return messageInterface.call(createProcessRequest, ProcessCreatedResponse.class, timeout);
+  }
+
+  @Override
+  public void createProcessAsync(CreateProcessRequest createProcessRequest,
+      ResponseCallback<ProcessCreatedResponse> callback) {
+    messageInterface.callAsync(createProcessRequest, ProcessCreatedResponse.class, callback);
+  }
+
+  @Override
+  public void subscribeCreateProcessRequest(MessageCallback<CreateProcessRequest> callback) {
+    messageInterface.subscribe(CreateProcessRequest.class, CreateProcessRequest.parser(), callback);
+  }
+
+  @Override
+  public ProcessCreatedResponse createLanceProcess(
+      CreateLanceProcessRequest createLanceProcessRequest) throws ResponseException {
+    return messageInterface.call(createLanceProcessRequest, ProcessCreatedResponse.class, timeout);
+  }
+
+  @Override
+  public void createLanceProcessAsync(CreateLanceProcessRequest createLanceProcessRequest,
+      ResponseCallback<ProcessCreatedResponse> callback) {
+    messageInterface.callAsync(createLanceProcessRequest, ProcessCreatedResponse.class, callback);
+  }
+
+  @Override
+  public void subscribeCreateLanceProcessRequest(
+      MessageCallback<CreateLanceProcessRequest> callback) {
+    messageInterface
+        .subscribe(CreateLanceProcessRequest.class, CreateLanceProcessRequest.parser(), callback);
   }
 
   @Override
