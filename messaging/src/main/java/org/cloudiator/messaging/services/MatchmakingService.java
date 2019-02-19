1095979e4943a87dc2742b2c0d4901712458d981
Daniel Baur <daniel.baur@uni-ulm.de>
2019-02-19 11:19:05 +0100

    added solution request

--- messaging/src/main/java/org/cloudiator/messaging/services/MatchmakingService.java
+++ messaging/src/main/java/org/cloudiator/messaging/services/MatchmakingService.java
@@ -4,6 +4,7 @@ import org.cloudiator.messages.entities.Matchmaking.MatchmakingRequest;
 import org.cloudiator.messages.entities.Matchmaking.MatchmakingResponse;
 import org.cloudiator.messages.entities.Matchmaking.NodeCandidateRequestMessage;
 import org.cloudiator.messages.entities.Matchmaking.NodeCandidateRequestResponse;
+import org.cloudiator.messages.entities.Matchmaking.SolutionRequest;
 import org.cloudiator.messaging.ResponseCallback;
 import org.cloudiator.messaging.ResponseException;
 
@@ -19,5 +20,7 @@ public interface MatchmakingService {
 
   void requestMatchAsync(MatchmakingRequest matchmakingRequest,
       ResponseCallback<MatchmakingResponse> response);
+
+  MatchmakingResponse retrieveSolution(SolutionRequest solutionRequest) throws ResponseException;
 
 }
