8cf96fc1769411a357e84c375549daccc79358ca
Volker Foth <volker.foth@uni-ulm.de>
2019-03-12 08:55:12 +0100

    changedIDgeneration

--- persistence/src/main/java/io/github/cloudiator/persistance/Model.java
+++ persistence/src/main/java/io/github/cloudiator/persistance/Model.java
@@ -23,6 +23,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
 
 /**
  * Superclass for all model classes. <p> Defines the auto generated id for each model class.
@@ -31,7 +32,28 @@ import javax.persistence.MappedSuperclass;
 abstract class Model implements Serializable {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.TABLE)
+  @GenericGenerator(
+      name = "sequenceGenerator",
+      strategy = "enhanced-sequence",
+      parameters = {
+          @org.hibernate.annotations.Parameter(
+              name = "optimizer",
+              value = "pooled-lo"
+          ),
+          @org.hibernate.annotations.Parameter(
+              name = "initial_value",
+              value = "1"
+          ),
+          @org.hibernate.annotations.Parameter(
+              name = "increment_size",
+              value = "6"
+          )
+      }
+  )
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "sequenceGenerator"
+  )
   private Long id;
 
   /**
