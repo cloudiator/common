25ac043bf438efa9a1fa556cd8e432bf2d6ebd92
Florian Held <florian.held@uni-ulm.de>
2019-05-24 15:03:29 +0200

    Implemented the method

--- messaging/src/main/java/org/cloudiator/messaging/services/ByonServiceImpl.java
+++ messaging/src/main/java/org/cloudiator/messaging/services/ByonServiceImpl.java
@@ -8,10 +8,12 @@ import org.cloudiator.messages.Byon.ByonNodeAllocateRequestMessage;
 import org.cloudiator.messages.Byon.ByonNodeAllocatedResponse;
 import org.cloudiator.messages.Byon.ByonNodeDeleteRequestMessage;
 import org.cloudiator.messages.Byon.ByonNodeDeletedResponse;
+import org.cloudiator.messages.Byon.ByonNodeEvent;
 import org.cloudiator.messages.Byon.ByonNodeQueryRequest;
 import org.cloudiator.messages.Byon.ByonNodeQueryResponse;
 import org.cloudiator.messages.Byon.ByonNodeRemovedResponse;
 import org.cloudiator.messages.Byon.RemoveByonNodeRequest;
+import org.cloudiator.messaging.MessageCallback;
 import org.cloudiator.messaging.MessageInterface;
 import org.cloudiator.messaging.ResponseCallback;
 import org.cloudiator.messaging.ResponseException;
@@ -62,5 +64,10 @@ public class ByonServiceImpl  implements ByonService {
   public ByonNodeQueryResponse findByonNodes(ByonNodeQueryRequest byonQueryRequest)
       throws ResponseException {
     return messageInterface.call(byonQueryRequest, ByonNodeQueryResponse.class, timeout);
+  }
+
+  @Override
+  public void subscribeByonNodeEvents(MessageCallback<ByonNodeEvent> callback) {
+    messageInterface.subscribe(ByonNodeEvent.class, ByonNodeEvent.parser(), callback);
   }
 }
