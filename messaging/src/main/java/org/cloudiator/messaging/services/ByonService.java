185a91a87601a97fbdb9f59e6f0bce3f4eae2425
Florian Held <florian.held@uni-ulm.de>
2019-05-24 15:01:10 +0200

    Added method to subscribe to ByonEvents

--- messaging/src/main/java/org/cloudiator/messaging/services/ByonService.java
+++ messaging/src/main/java/org/cloudiator/messaging/services/ByonService.java
@@ -6,10 +6,12 @@ import org.cloudiator.messages.Byon.ByonNodeAllocateRequestMessage;
 import org.cloudiator.messages.Byon.ByonNodeAllocatedResponse;
 import org.cloudiator.messages.Byon.ByonNodeDeleteRequestMessage;
 import org.cloudiator.messages.Byon.ByonNodeDeletedResponse;
+import org.cloudiator.messages.Byon.ByonNodeEvent;
 import org.cloudiator.messages.Byon.ByonNodeQueryRequest;
 import org.cloudiator.messages.Byon.ByonNodeQueryResponse;
 import org.cloudiator.messages.Byon.ByonNodeRemovedResponse;
 import org.cloudiator.messages.Byon.RemoveByonNodeRequest;
+import org.cloudiator.messaging.MessageCallback;
 import org.cloudiator.messaging.ResponseCallback;
 import org.cloudiator.messaging.ResponseException;
 
@@ -24,4 +26,5 @@ public interface ByonService {
       ResponseCallback<ByonNodeRemovedResponse> callback);
   ByonNodeQueryResponse findByonNodes(ByonNodeQueryRequest byonQueryRequest)
       throws ResponseException;
+  void subscribeByonNodeEvents(MessageCallback<ByonNodeEvent> callback);
 }
